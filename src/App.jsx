52f121269ddebce3c917bae7ad5758a48cf3a5f9
Skinng <felipefaundez.vasquez@gmail.com>
2024-12-15 12:42:25 -0300

    actualización de import y componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
-import Cardpizza from './components/Cardpizza'
 import Footer from './components/Footer'
 import Home from './components/Home'
 
